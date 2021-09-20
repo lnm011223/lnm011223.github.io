56a6d4b735297cd02f3fb61a601b3ebc319f2552
lnm011223 <2969827073@qq.com>
2021-09-21 02:02:59 +0800

    Site updated: 2021-09-21 02:02:59

--- js/Lete.js
+++ js/Lete.js
@@ -1,3 +1,12 @@
+/**
+ * Created by Lete on 2020/7/7.
+ * Butterfly在3.4.0版本后去除了jQuery
+ * 注意有使用到jQuery，请自行引入
+ * 不想用jQuery可自行更改
+ */
+
+
+
 // 可爱的Title
 var OriginTitle = document.title;
 var titleTime;
@@ -15,6 +24,7 @@ document.addEventListener('visibilitychange', function() {
     }
 });
 
+
 // 气泡
 function qipao() {
     $('#page-header').circleMagic({
@@ -23,45 +33,11 @@ function qipao() {
         color: 'rgba(255,255,255,.4)',
         clearOffset: 0.99
     });
-}! function(p) {
-    p.fn.circleMagic = function(t) {
-        var o, a, n, r, e = !0,
-            i = [],
-            d = p.extend({ color: "rgba(255,0,0,.5)", radius: 10, density: .3, clearOffset: .2 }, t),
-            l = this[0];
+}!function(a){a.fn.circleMagic=function(y){var g,x,j,b,u=!0,m=[],v=a.extend({color:"rgba(255,0,0,.5)",radius:10,density:0.3,clearOffset:0.2},y),k=this[0];function w(){u=!(document.body.scrollTop>x)}function z(){g=k.clientWidth,x=k.clientHeight,k.height=x+"px",j.width=g,j.height=x}function p(){if(u){for(var c in b.clearRect(0,0,g,x),m){m[c].draw()}}requestAnimationFrame(p)}function q(){var c=this;function d(){c.pos.x=Math.random()*g,c.pos.y=x+100*Math.random(),c.alpha=0.1+Math.random()*v.clearOffset,c.scale=0.1+0.3*Math.random(),c.speed=Math.random(),"random"===v.color?c.color="rgba("+Math.floor(255*Math.random())+", "+Math.floor(0*Math.random())+", "+Math.floor(0*Math.random())+", "+Math.random().toPrecision(2)+")":c.color=v.color}c.pos={},d(),this.draw=function(){c.alpha<=0&&d(),c.pos.y-=c.speed,c.alpha-=0.0005,b.beginPath(),b.arc(c.pos.x,c.pos.y,c.scale*v.radius,0,2*Math.PI,!1),b.fillStyle=c.color,b.fill(),b.closePath()}}!function(){g=k.offsetWidth,x=k.offsetHeight,function(){var e=document.createElement("canvas");e.id="canvas",e.style.top=0,e.style.zIndex=0,e.style.position="absolute",k.appendChild(e),e.parentElement.style.overflow="hidden"}(),(j=document.getElementById("canvas")).width=g,j.height=x,b=j.getContext("2d");for(var c=0;c<g*v.density;c++){var d=new q;m.push(d)}p()}(),window.addEventListener("scroll",w,!1),window.addEventListener("resize",z,!1)}}(jQuery);
+qipao()
 
-        function c() { e = !(document.body.scrollTop > a) }
 
-        function s() { o = l.clientWidth, a = l.clientHeight, l.height = a + "px", n.width = o, n.height = a }
 
-        function h() {
-            if (e)
-                for (var t in r.clearRect(0, 0, o, a), i) i[t].draw();
-            requestAnimationFrame(h)
-        }
-
-        function f() {
-            var t = this;
-
-            function e() { t.pos.x = Math.random() * o, t.pos.y = a + 100 * Math.random(), t.alpha = .1 + Math.random() * d.clearOffset, t.scale = .1 + .3 * Math.random(), t.speed = Math.random(), "random" === d.color ? t.color = "rgba(" + Math.floor(255 * Math.random()) + ", " + Math.floor(0 * Math.random()) + ", " + Math.floor(0 * Math.random()) + ", " + Math.random().toPrecision(2) + ")" : t.color = d.color }
-            t.pos = {}, e(), this.draw = function() { t.alpha <= 0 && e(), t.pos.y -= t.speed, t.alpha -= 5e-4, r.beginPath(), r.arc(t.pos.x, t.pos.y, t.scale * d.radius, 0, 2 * Math.PI, !1), r.fillStyle = t.color, r.fill(), r.closePath() }
-        }! function() {
-            o = l.offsetWidth, a = l.offsetHeight,
-                function() {
-                    var t = document.createElement("canvas");
-                    t.id = "canvas", t.style.top = 0, t.style.zIndex = 0, t.style.position = "absolute", l.appendChild(t), t.parentElement.style.overflow = "hidden"
-                }(), (n = document.getElementById("canvas")).width = o, n.height = a, r = n.getContext("2d");
-            for (var t = 0; t < o * d.density; t++) {
-                var e = new f;
-                i.push(e)
-            }
-            h()
-        }(), window.addEventListener("scroll", c, !1), window.addEventListener("resize", s, !1)
-    }
-}(jQuery);
-
-// 调用气泡方法
-qipao();
 
 
 /* 手机客户端导航栏默认隐藏 */
